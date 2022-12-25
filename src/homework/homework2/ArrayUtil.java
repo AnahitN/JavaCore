56d3efbd9c790dcf3a1ea127288f55a79abe4b69
homework <tanahit360@gmail.com>
2022-12-25 23:50:18 +0400

    3rd homework

--- src/homework/homework2/ArrayUtil.java
+++ src/homework/homework2/ArrayUtil.java
@@ -29,6 +29,49 @@ public class ArrayUtil {
 
         }
         System.out.println("մասիվի ամենափոքր թիվը " + x + " է։");
+
+        System.out.println("մասիվի զույգ  թվերն են՝");
+        int i;
+        int even = 0;
+        for (i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 0) {
+                even++;
+
+                System.out.print(array[i] + " ");
+            }
+        }
+        System.out.println();
+        System.out.println("զույգ թվերի քանակը " + even + " է։");
+
+        System.out.println("մասիվի կենտ թվերն են՝");
+        int odd = 0;
+        for (int j = 0; j < array.length; j++) {
+            if (array[j] % 2 != 0) {
+                System.out.print(array[j] + " ");
+                odd++;
+            }
+
+        }
+        System.out.println();
+        System.out.println("կենտ թվերի քանակը " + odd + " է։");
+
+
+        int result = 0;
+        for (int j = 0; j < array.length; j++) {
+            result = result + array[j];
+
+        }
+        System.out.println("Միջին թվաբանականը հավասար է " + result / 5 + "-ի");
+
+        System.out.println("Մասիվի էլեմենտների գումարը հավասար է " + result + "-ի");
+        System.out.println("մասիվի առաջին էլեմենտը " + array[0] + "ն է։");
+        System.out.println("մասիվի վերջին էլեմենտը " + array[array.length - 1] + "ն է։");
+        System.out.println("մասիվի մեջտեղի էլեմենտը " + array[array.length / 2] + "ն է։");
     }
+
+
 }
+
+
+
 
